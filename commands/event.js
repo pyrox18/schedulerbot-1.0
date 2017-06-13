0c7181665f8f84ce670003140489ce9a2d2c396b
pyrox18 <haryzizzudin@gmail.com>
2017-06-13 18:13:11 +0800

    Make event listing, update and delete as subcommands of event command

--- commands/event.js
+++ commands/event.js
@@ -8,7 +8,7 @@ const config = require('../config/bot');
 const prefix = config.prefix;
 
 module.exports = (bot) => {
-  bot.registerCommand("event", (msg, args) => {
+  let eventCommand = bot.registerCommand("event", (msg, args) => {
     if (args.length < 1) {
       return "Invalid input.";
     }
@@ -48,5 +48,107 @@ module.exports = (bot) => {
     description: "Add a new event.",
     fullDescription: "Adds a new event to the guild calendar. Type the event details naturally (e.g. 'CS:GO scrims tomorrow from 6pm to 9pm') and the bot will interpret it for you.",
     usage: "`<event details>`"
+  });
+
+  eventCommand.registerSubcommand("list", (msg, args) => {
+    if (args.length > 0) {
+      return "Invalid input.";
+    }
+    
+    let calendars = new Calendars();
+
+    events = calendars.getEventsForCalendar(msg.channel.guild.id);
+
+    if (events === null) {
+      return "Calendar not initialized. Please run `" + prefix + "calendar` to initialize the calendar first.";
+    }
+    else {
+      resultString = "```css\n[Events]\n\n";
+      for (let i = 0; i < events.length; i++) {
+        resultString = resultString + `${i+1} : ${events[i].name} /* ${events[i].startDate} to ${events[i].endDate} */\n`;
+      }
+
+      resultString = resultString + "```";
+      return resultString;
+    }
+  }, {
+    description: "List existing events.",
+    fullDescription: "Displays a list of events that have been created."
+  });
+
+  eventCommand.registerSubcommand("delete", (msg, args) => {
+    if (args.length < 1 || args.length > 1) {
+      return "Invalid input.";
+    }
+    
+    let calendars = new Calendars();
+
+    let index = parseInt(args[0]);
+    if (isNaN(index)) {
+      return "Invalid input.";
+    }
+    index = index - 1;
+
+    let resultCode = calendars.deleteEvent(msg.channel.guild.id, index);
+    switch (resultCode) {
+      case -1:
+        return "Calendar not initialized. Please run `" + prefix + "calendar` to initialize the calendar first.";
+      case 0:
+        return "Event not found.";
+      default:
+        return "Event successfully deleted.";
+    }
+  }, {
+    description: "Delete an event.",
+    fullDescription: "Delete an event from the existing event list.",
+    usage: "`<event number>`"
+  });
+
+  eventCommand.registerSubcommand("update", (msg, args) => {
+    if (args.length < 2) {
+      return "Invalid input.";
+    }
+
+    let calendars = new Calendars();
+
+    let index = parseInt(args[0]);
+    if (isNaN(index)) {
+      return "Invalid input.";
+    }
+    index = index - 1;
+
+    let inputString = args.slice(1).join(" ");
+    let results = chrono.parse(inputString);
+
+    let eventName = inputString.replace(results[0].text, "").trim();
+    let startDate = moment(results[0].start.date());
+    let endDate;
+    try {
+      endDate = moment(results[0].end.date());
+    }
+    catch (err) {
+      endDate = startDate.clone().add(1, 'h');
+    }
+
+    let updateSuccessObject = calendars.updateEvent(msg.channel.guild.id, index, eventName, startDate, endDate);
+
+    if (updateSuccessObject === null) {
+      return "Calendar not initialized or event not found.";
+    }
+    else {
+      let embed = bot.createEmbed(msg.channel.id);
+      embed.title("New Event");
+      embed.color(0xfff835);
+      embed.field("Event Name", eventName, false);
+      embed.field("Start Date", updateSuccessObject.actualStartDate.format('MMM D YYYY, h:mm:ss a z'), false);
+      embed.field("End Date", updateSuccessObject.actualEndDate.format('MMM D YYYY, h:mm:ss a z'), false);
+
+      embed.send(bot, msg.channel.id);
+      return "Event updated.";
+    }
+  }, {
+    description: "Update an existing event.",
+    fullDescription: "Updates an existing event in the guild calendar.",
+    usage: "`<event number> <event details>`"
   });
 }
