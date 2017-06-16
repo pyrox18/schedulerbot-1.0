5905c9dc5bd8dd6903ee0621fde7d841dcbc4975
pyrox18 <haryzizzudin@gmail.com>
2017-06-16 16:42:22 +0800

    Add dbConnectionUrl to bot config example

--- config/bot-example.config.js
+++ config/bot-example.config.js
@@ -4,5 +4,6 @@ module.exports = {
   prefix: '+', // You can change this to something else to not conflict with other bots in your server
   game: {
     name: "+help | In development" // Change the bot's "Playing" status on Discord; see https://abal.moe/Eris/docs/CommandClient#function-editStatus
-  }
+  },
+  dbConnectionUrl: 'mongodb://localhost:27017/schedulerbot' // URL to connect to MongoDB
 }
