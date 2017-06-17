e67034d7d1e2ba7e2005f156be52eb334ebe200a
pyrox18 <haryzizzudin@gmail.com>
2017-06-18 00:28:47 +0800

    Implement custom guild prefixes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 
 const config = require('./config/bot.config');
+const Prefix = require('./models/prefix.model');
 
 const prefix = config.prefix;
 
@@ -28,15 +29,44 @@ bot = new Eris.CommandClient(config.botToken, {}, {
 });
 
 bot.on("ready", () => {
+  // Register commands
   glob.sync('./commands/**/*.command.js').forEach(file => {
     const actualCommand = require(path.resolve(file));
     actualCommand(bot);
+  });
+
+  // Load all guild prefixes
+  Prefix.find((err, prefixes) => {
+    for (let guild of prefixes) {
+      let prefixes = [guild.prefix, bot.user.mention + " "];
+      bot.registerGuildPrefix(guild.guildId, prefixes);
+    }
   });
 
   // Set bot's Playing text
   bot.editStatus(config.game);
 
   console.log("Bot ready!");
+});
+
+bot.on('guildCreate', guild => {
+  // Create new Prefix document when joining a guild
+  let newGuild = new Prefix({
+    guildId: guild.id,
+    prefix: config.prefix
+  });
+  newGuild.save(err => {
+    if (err) {
+      console.error(err);
+    }
+  })
+});
+
+bot.on('guildDelete', guild => {
+  // Delete Prefix document of guild when leaving a guild
+  Prefix.findOneAndRemove({ guildId: guild.id }, (error, document) => {
+    console.log(`Deleted guildId ${document.guildId} from Prefix collection`);
+  });
 });
 
 bot.connect();
--- commands/misc.command.js
+++ commands/misc.command.js
@@ -1,6 +1,7 @@
 const moment = require('moment-timezone');
 
 const config = require('../config/bot.config');
+const Prefix = require('../models/prefix.model');
 
 let prefix = config.prefix;
 
@@ -13,9 +14,37 @@ module.exports = (bot) => {
     fullDescription: "Pings the bot to check if the bot is available."
   });
 
-  bot.registerCommand("prefix", "`" + prefix + "`", {
-    description: "Show prefix.",
-    fullDescription: "Shows the bot's current prefix.",
-    usage: "(or `@SchedulerBot prefix` if you forgot what the prefix is)"
+  bot.registerCommand("prefix", (msg, args) => {
+    if (args.length == 0) {
+      Prefix.findByGuildId(msg.channel.guild.id, (err, prefix) => {
+        if (err) {
+          console.error(err);
+        }
+        else {
+          msg.channel.createMessage("`" + prefix.prefix + "`");
+        }
+      });
+    }
+    else if (args.length > 1) {
+      msg.channel.createMessage("Invalid input.");
+    }
+    else {
+      Prefix.findByGuildId(msg.channel.guild.id, (err, prefix) => {
+        prefix.updatePrefix(args[0], err => {
+          if (err) {
+            console.error(err);
+          }
+          else {
+            prefixes = [args[0], bot.user.mention + " "];
+            bot.registerGuildPrefix(msg.channel.guild.id, prefixes);
+            msg.channel.createMessage("Prefix set to `" + args[0] + "`.");
+          }
+        });
+      });
+    }
+  }, {
+    description: "Show or set prefix.",
+    fullDescription: "Shows the bot's current prefix when called without arguments. To set a new prefix for the guild, call the command with the desired prefix after the command. You can also call the command by mentioning the bot (`@SchedulerBot prefix`) if you forget what the prefix is.",
+    usage: "[new prefix]"
   });
 }
