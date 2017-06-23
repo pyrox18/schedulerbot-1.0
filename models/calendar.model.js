2962c366d65b916cbfba90e225e8b68e17ef0eb4
pyrox18 <haryzizzudin@gmail.com>
2017-06-23 16:52:09 +0800

    Retain ObjectID when updating events

--- models/calendar.model.js
+++ models/calendar.model.js
@@ -58,8 +58,32 @@ calendarSchema.methods.deleteEvent = function(eventIndex, callback) {
 
 calendarSchema.methods.updateEvent = function(eventIndex, eventName, startDate, endDate, callback) {
   if (eventIndex >= 0 && eventIndex < this.events.length) {
-    this.events.splice(eventIndex, 1);
-    this.addEvent(eventName, startDate, endDate, callback);
+    let eventArray = this.events.splice(eventIndex, 1);
+    let event = eventArray[0];
+    let actualStartDate = moment.tz(startDate.format('YYYY-MM-DDTHH:mm:ss.SSS'), moment.ISO_8601, this.timezone);
+    let actualEndDate = moment.tz(endDate.format('YYYY-MM-DDTHH:mm:ss.SSS'), moment.ISO_8601, this.timezone);
+
+    event.name = eventName;
+    event.startDate = actualStartDate.toISOString();
+    event.endDate = actualEndDate.toISOString();
+
+    if (this.events.length == 0) {
+      this.events.push(event);
+    }
+    else {
+      for (let i = 0; i < this.events.length; i++) {
+        if (this.events[i].startDate >= event.startDate) {
+          this.events.splice(i, 0, event);
+          break;
+        }
+        if (i == this.events.length - 1) {
+          this.events.push(event);
+          break;
+        }
+      }
+    }
+
+    this.save(callback);
   }
 }
 
