e28fc8cb9646aae92da7ccf1495290bb41692432
pyrox18 <haryzizzudin@gmail.com>
2017-07-21 09:59:40 +0800

    Display message ID in error message

--- models/command-error.model.js
+++ models/command-error.model.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 
 class CommandError {
   constructor(err, bot, msg) {
-    bot.createMessage(msg.channel.id, "An error has occurred.\n```" + err + "\n```");
+    bot.createMessage(msg.channel.id, "An error has occurred. Please provide the message ID to the support server.\n```javascript\nMessage ID: " + msg.id + "\n" + err + "\n```");
     winston.log('error', err, { 
       guildId: msg.channel.guild.id,
       msg: {
