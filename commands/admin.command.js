946c956440f17cb62c8f5800c4e49454248bb745
pyrox18 <haryzizzudin@gmail.com>
2017-07-24 18:48:24 +0800

    Remove admin commands temporarily due to commands showing up in help command

--- commands/admin.command.js
+++ commands/admin.command.js
@@ -2,22 +2,22 @@ const CommandError = require('../models/command-error.model');
 const config = require('../config/bot.config');
 
 module.exports = (bot) => {
-  bot.registerCommand('admincheck', (msg, args) => {
-    if (isAdmin(msg.member.id)) {
-      return "Yes";
-    }
-    return;
-  });
+  // bot.registerCommand('admincheck', (msg, args) => {
+  //   if (isAdmin(msg.member.id)) {
+  //     return "Yes";
+  //   }
+  //   return;
+  // });
 
-  bot.registerCommand('forceerror', (msg, args) => {
-    if (!isAdmin(msg.member.id)) return;
-    try {
-      throw new Error('test error');
-    }
-    catch (err) {
-      new CommandError(err, bot, msg);
-    }
-  });
+  // bot.registerCommand('forceerror', (msg, args) => {
+  //   if (!isAdmin(msg.member.id)) return;
+  //   try {
+  //     throw new Error('test error');
+  //   }
+  //   catch (err) {
+  //     new CommandError(err, bot, msg);
+  //   }
+  // });
 }
 
 function isAdmin(id) {
