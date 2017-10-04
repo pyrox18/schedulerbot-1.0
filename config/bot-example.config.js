526a7d3e40ff6ce9a867ccc465a87d4f066454a0
pyrox18 <haryzizzudin@gmail.com>
2017-10-05 02:20:04 +0800

    Delete old source files

--- config/bot-example.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  botToken: "copyAndPasteYourEntireAppBotUserTokenHere", // Replace the string with your token
-  prefix: '+', // You can change this to something else to not conflict with other bots in your server
-  game: {
-    name: "+help | In development" // Change the bot's "Playing" status on Discord; see https://abal.moe/Eris/docs/CommandClient#function-editStatus
-  },
-  dbConnectionUrl: 'mongodb://localhost:27017/schedulerbot', // URL to connect to MongoDB,
-  adminId: "1234567890123456" // Bot owner ID for hidden admin commands
-}
