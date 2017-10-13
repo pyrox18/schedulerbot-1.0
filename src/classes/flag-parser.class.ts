a17246eff45d8793e04aa9f6e400e096a46a31be
pyrox18 <haryzizzudin@gmail.com>
2017-10-13 16:13:38 +0800

    Add command body parsing to flag parser

--- src/classes/flag-parser.class.ts
+++ src/classes/flag-parser.class.ts
@@ -4,6 +4,16 @@ export class FlagParser {
   public static parse(args: string[]): any {
     let flagData: any = {};
     let i: number = 0;
+    let body: string[] = [];
+
+    // Push the arg as body text until the first flag is detected
+    while (i < args.length && !args[i].startsWith('--')) {
+      body.push(args[i]);
+      i++;
+    }
+    flagData._body = body.join(' ');
+
+    // Parse the flags in args
     while (i < args.length) {
       if (args[i].includes('--')) {
         let key: string = args[i].slice(2);
