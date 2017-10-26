d3290abba6cdb23eb0725cd78fc07ddc81aac04c
pyrox18 <haryzizzudin@gmail.com>
2017-10-26 17:47:45 +0800

    Add eval command

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,4 +1,5 @@
 import { Message, CommandOptions } from 'eris';
+import { inspect } from 'util';
 
 import { CommandController } from './command.controller';
 import { CommandError } from '../classes/command-error.class';
@@ -30,10 +31,39 @@ export class AdminController extends CommandController {
     }
   }
 
+  public eval = (msg: Message, args: string[]): string => {
+    if (msg.author.id !== config.adminId) return; // safety
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string") {
+        evaled = inspect(evaled);
+      }
+
+      let output: string = "```js\n" + this.clean(evaled) + "\n```";
+      if (output.length > 1990) {
+        output = output.substr(0, 1986);
+        output += "\nTruncated\n```"
+      }
+      return output;
+    } catch (err) {
+      return `\`ERROR\` \`\`\`xl\n${this.clean(err)}\n\`\`\``
+    }
+  }
+
   public registerCommands(): boolean {
     this.bot.registerCommand("admincheck", this.adminCheck, this.commandOptions);
     this.bot.registerCommand("forceerror", this.forceError, this.commandOptions);
+    this.bot.registerCommand("eval", this.eval, this.commandOptions);
     return true;
+  }
+
+  private clean(evalResult: any): string {
+    if (typeof(evalResult) === "string") {
+      return evalResult.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    }
+    return evalResult;
   }
 }
 
